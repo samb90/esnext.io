51b0d3525527601b7dcd22de1a0936b8f204ad65
David White <david.white@spry-soft.com>
2016-06-29 22:49:53 +0100

    Started work towards adding JS runners for current browser and babel environments

---
+++ es2016/index.md
@@ -0,0 +1,13 @@
+---
+layout: page
+title: ES2016
+---
+
+## Exponential operators
+
+No real content here yet...
+
+{% highlight javascript %}
+const squared = 2 ** 2;
+const cubed = 2 ** 3;
+{% endhighlight %}
--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 (function (window, document) {
   document.addEventListener("DOMContentLoaded", function (event) {
-    console.log("DOM fully loaded and parsed");
+
+    var codes = document.querySelectorAll('.highlight');
+    codes.forEach(function (code) {
+      var runner = document.createElement('div')
+      runner.className = "runner";
+
+      var browserButton = document.createElement('button');
+      browserButton.className = "button browser";
+      browserButton.textContent = "Run in browser";
+
+      var babelButton = document.createElement('button');
+      babelButton.className = "button babel";
+      babelButton.textContent = "Run with Babel";
+
+      runner.appendChild(browserButton);
+      runner.appendChild(babelButton);
+
+      code.appendChild(runner);
+    });
   });
 })(window, document, undefined);
